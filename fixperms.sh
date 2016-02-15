73ffe7ccd61fc33f0ba337bd6a92cf1caaf009cc
Uli Straub <uli.straub@kontinui.de>
2016-02-15 22:16:45 +0100

    updated permissions & webhook file

--- fixperms.sh
+++
@@ -1,7 +0,0 @@
-chown ulliver:staff .
-chown -R ulliver:staff *
-find . -type f ! -path "./bin/" | xargs chmod 664
-find . -type f -path "./bin/" | xargs chmod 775
-find . -type d | xargs chmod 775
-find . -type d | xargs chmod +s
-umask 0002
--- permission-script.sh
+++ permission-script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-chown ulliver:staff .
-chown -R ulliver:staff *
+chown toctoc:toctoc .
+chown -R toctoc:toctoc *
 find . -type f | xargs chmod 664
 find ./bin -type f | xargs chmod 775
 find . -type d | xargs chmod 775
