2d4dfd3b8b31f0b11ca33736b20eebf9c1ee617c
Uli Straub <uli.straub@kontinui.de>
2016-03-31 01:09:42 +0200

    ie objectfit-fix reset js and css

--- user/themes/tictactoc/js/msie.js
+++ user/themes/tictactoc/js/msie.js
@@ -1,19 +1,19 @@
 // if ( ! Modernizr.objectfit ) {
 
 
-if('objectFit' in document.documentElement.style === false) {
+// if('objectFit' in document.documentElement.style === false) {
 
-  console.log("this be IE");
+//   console.log("this be IE");
 
-  $('.image-row').each(function () {
-    console.log("found image-row");
-    var $container = $(this),
-        imgUrl = $container.find('img').prop('src');
-    if (imgUrl) {
-      console.log("found img src");
-      $container.find('a')
-        .css('backgroundImage', 'url(' + imgUrl + ')')
-        .addClass('compat-object-fit');
-    }  
-  });
-}
+//   $('.image-row').each(function () {
+//     console.log("found image-row");
+//     var $container = $(this),
+//         imgUrl = $container.find('img').prop('src');
+//     if (imgUrl) {
+//       console.log("found img src");
+//       $container.find('a')
+//         .css('backgroundImage', 'url(' + imgUrl + ')')
+//         .addClass('compat-object-fit');
+//     }  
+//   });
+// }
