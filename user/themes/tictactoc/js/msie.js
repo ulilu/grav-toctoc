b42fa8c2f1560ea2885967ce4b1ef0fc4d3bbec3
Uli Straub <uli.straub@kontinui.de>
2016-03-31 00:48:48 +0200

    ieo

--- user/themes/tictactoc/js/msie.js
+++ user/themes/tictactoc/js/msie.js
@@ -5,13 +5,13 @@ if('objectFit' in document.documentElement.style === false) {
 
   alert("this be IE");
 
-  $('.image-row a').each(function () {
-    alert("found image-row a");
+  $('.image-row').each(function () {
+    alert("found image-row");
     var $container = $(this),
         imgUrl = $container.find('img').prop('src');
     if (imgUrl) {
       alert("found img src");
-      $container
+      $container.find('a')
         .css('backgroundImage', 'url(' + imgUrl + ')')
         .addClass('compat-object-fit');
     }  
