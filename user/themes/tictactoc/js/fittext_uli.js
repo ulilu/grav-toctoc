5aca900a01225e57cace3a7ffd3c5ea353447731
Uli Straub <uli.straub@kontinui.de>
2016-03-30 16:09:41 +0200

    fittext iOS debugging

--- user/themes/tictactoc/js/fittext_uli.js
+++ user/themes/tictactoc/js/fittext_uli.js
@@ -20,39 +20,36 @@
           'maxFontSize' : Number.POSITIVE_INFINITY
         }, options);
 
+    // Store the window width
+    var windowWidth = $(window).width();
+    // console.log("original window-width =" + windowWidth);
+
     return this.each(function(){
 
       // Store the object
       var $this = $(this);
 
-      // Store the window width
-      var windowWidth = $(window).width();
-
       // Resizer() resizes items based on the object width divided by the compressor * 10
-      var resizer = function(firstRun) {
+      var resizer = function(ok) {
 
         // Check window width has actually changed and it's not just iOS triggering a resize event on scroll
-        if ($(window).width() != windowWidth | firstRun) {
-            alert("firstrun =" + firstRun);
+        // Always run on initial call and orientationchange event
+        if ($(window).width() != windowWidth | ok == true ) {
+            console.log("firstrun =" + ok);
+            
             // Update the window width for next time
-            alert("window-width =" + windowWidth);
             windowWidth = $(window).width();
-            alert("NEW WINDOW-WIDTH =" + windowWidth);
+            // console.log("NEW WINDOW-WIDTH =" + windowWidth);
 
             $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
         }
       };
 
-      var sizechanger = function () {
-        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-      };
-
       // Call once to set.
-      resizer(1);
+      resizer(true);
 
       // Call on resize. Opera debounces their resize by default.
-      $(window).on('resize.fittext', resizer);
-      $(window).on('orientationchange.fittext', sizechanger);
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
     });
 
   };
