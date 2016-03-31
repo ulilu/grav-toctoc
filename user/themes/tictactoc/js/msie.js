df821ffa966769c2ef85b06f92240d816948b916
Uli Straub <uli.straub@kontinui.de>
2016-03-31 02:19:50 +0200

    hun

--- user/themes/tictactoc/js/msie.js
+++ user/themes/tictactoc/js/msie.js
@@ -1,34 +1,37 @@
 // if ( ! Modernizr.objectfit ) {
 
+document.addEventListener("DOMContentLoaded", function(event) { 
 
-// Detect objectFit support
-if('objectFit' in document.documentElement.style === false) {
-  
-  // assign HTMLCollection with parents of images with objectFit to variable
-  var container = document.querySelector('figure a');
+  // Detect objectFit support
+  if('objectFit' in document.documentElement.style === false) {
+    
+    // assign HTMLCollection with parents of images with objectFit to variable
+    var container = document.querySelectorAll('figure a');
 
-  alert(container);
-  
-  // Loop through HTMLCollection
-  for(var i = 0; i < container.length; i++) {
+    alert(container);
     
-    // Asign image source to variable
-    var imageSource = container[i].querySelector('img').src;
-    
-    // Hide image
-    container[i].querySelector('img').style.opacity = '0';
-    
-    // Add background-size: cover
-    container[i].style.backgroundSize = 'cover';
-    
-    // Add background-image: and put image source here
-    container[i].style.backgroundImage = 'url(' + imageSource + ')';
-    
-    // Add background-position: center center
-    container[i].style.backgroundPosition = 'center center';
+    // Loop through HTMLCollection
+    for(var i = 0; i < container.length; i++) {
+      
+      // Asign image source to variable
+      var imageSource = container[i].querySelector('img').src;
+      
+      // Hide image
+      container[i].querySelector('img').style.opacity = '0';
+      
+      // Add background-size: cover
+      container[i].style.backgroundSize = 'cover';
+      
+      // Add background-image: and put image source here
+      container[i].style.backgroundImage = 'url(' + imageSource + ')';
+      
+      // Add background-position: center center
+      container[i].style.backgroundPosition = 'center center';
+    }
   }
-}
-else {
-  // You don't have to worry
-  console.log('No worries, your browser supports objectFit')
-}
+  else {
+    // You don't have to worry
+    console.log('No worries, your browser supports objectFit')
+  }
+
+});
