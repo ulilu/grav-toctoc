7484144b4b32ef7d89f5863ea7ff826d630b063f
Uli Straub <uli.straub@kontinui.de>
2016-03-31 02:05:03 +0200

    nui

--- user/themes/tictactoc/js/msie.js
+++ user/themes/tictactoc/js/msie.js
@@ -5,7 +5,7 @@
 if('objectFit' in document.documentElement.style === false) {
   
   // assign HTMLCollection with parents of images with objectFit to variable
-  var container = document.getElementsByClassName('image-row').getElementsByTagName('a');
+  var container = document.querySelector('.image-row a');
 
   alert(container);
   
