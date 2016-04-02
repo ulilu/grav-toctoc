77663116dfa27bdce6b61cf20f8bd798be430ae3
Uli Straub <uli.straub@kontinui.de>
2016-04-03 01:04:30 +0200

    fixing menu & light slider (with new layout)

--- user/themes/tictactoc/js/scripts.js
+++ user/themes/tictactoc/js/scripts.js
@@ -13,11 +13,56 @@
 //     });
 // });
 
+
+
 /*
  * Document ready start
  */
 
 $(document).ready(function() {
+
+
+  /*
+   * Center active menu item
+   */
+
+  var activeMenu = function() {
+    
+    if ( $('.main-nav__item.active').length ) {
+
+      var navi = $('.main-nav');
+      var target = $('.main-nav__item.active'); 
+
+      var w = navi.width();
+      // console.log("navi.width = " + w );
+
+      var x = navi[0].scrollWidth;
+      // console.log("navi.scrollwidth = " + x );
+      
+      var y = target.outerWidth(true);
+      // console.log("target.outerWidth = " + y );
+
+      var p = target.position().left;
+      // console.log("target.position = " + p );
+
+      var t = (((w / 2) - (y / 2)) - p); 
+      // console.log("move by = " + t );
+
+      navi.scrollLeft( -t );
+
+    }
+    
+  };
+
+  activeMenu();
+
+
+  $('.main-nav__link').click(function(){
+    $('.main-nav__item.active').removeClass("active");
+    $(this).addClass("active");
+  });
+
+
 
   jQuery(".row-with-title figcaption").fitText(0.4, { minFontSize: '24px', maxFontSize: '42px' });
 
@@ -55,12 +100,13 @@ $(document).ready(function() {
      * Button scroll block
      */
 
-    var firstItem = $('a.card:first-of-type');
-    var fip = firstItem.position().left;
-    var fipi = fip; // initial offset for first card
+    if ( $('.js-scrollblock').length ) {
+      var firstItem = $('a.card:first-of-type');
+      var fip = firstItem.position().left;
+      var fipi = fip; // initial offset for first card
+    }
 
     $('.js-scrollblock').click(function(){
-
       var wrapper = $('.block__scroll-wrapper');
       var ww = wrapper[0].scrollWidth;
       var sw = $( window ).width();
@@ -102,34 +148,4 @@ $(document).ready(function() {
         // document.activeElement = null;
     });
 
-
-    /*
-     * Center active menu item
-     */
-
-		var navi = $('.main-nav');
-    var target = $('.main-nav__item.active'); 
-
-    var w = navi.width();
-		// console.log("navi.width = " + w );
-
-		var x = navi[0].scrollWidth;
-		// console.log("navi.scrollwidth = " + x );
-		
-		var y = target.outerWidth(true);
-		// console.log("target.outerWidth = " + y );
-
-		var p = target.position().left;
-		// console.log("target.position = " + p );
-
-		var t = (((w / 2) - (y / 2)) - p); 
-		// console.log("move by = " + t );
-
-		navi.scrollLeft( -t );
-
-
-		$('.main-nav__link').click(function(){
-			$('.main-nav__item.active').removeClass("active");
-			$(this).addClass("active");
-		});
 });
