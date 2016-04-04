d011ea0abaca169020bd4c079e2f2aaeb9afd839
Uli Straub <uli.straub@kontinui.de>
2016-04-04 06:48:08 +0200

    modular how it works

--- user/themes/tictactoc/js/scripts.js
+++ user/themes/tictactoc/js/scripts.js
@@ -68,85 +68,105 @@ $(document).ready(function() {
   jQuery(".row-with-title figcaption").fitText(0.4, { minFontSize: '24px', maxFontSize: '42px' });
 
 
-    /*
-     * Button scroll to top
-     */
-    var id = 'js-scrolltop';
-    $('.' + id).click(function(){
-        $('html, body').animate({ scrollTop: 0 }, 150);
+
+  var $timeline_block = $('.timeline-row');
+
+  //hide timeline blocks which are outside the viewport
+  $timeline_block.each(function(){
+    if($(this).offset().top > $(window).scrollTop()+$(window).height()*0.75) {
+      $(this).find('> p, .timeline-row__info').addClass('is-hidden');
+    }
+  });
+
+  //on scolling, show/animate timeline blocks when enter the viewport
+  $(window).on('scroll', function(){
+    $timeline_block.each(function(){
+      if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('> p').hasClass('is-hidden') ) {
+        $(this).find('> p, .timeline-row__info, .timeline-row__info').removeClass('is-hidden').addClass('bounce-in');
+      }
     });
-    window.onscroll = function () {
-        if(window.pageYOffset < 1000) {
-            $('.' + id).fadeOut(300);
-        }
-        else {
-            $('.' + id).fadeIn(300);
-        }
+  });
+
+
+  /*
+   * Button scroll to top
+   */
+  var id = 'js-scrolltop';
+  $('.' + id).click(function(){
+      $('html, body').animate({ scrollTop: 0 }, 150);
+  });
+  window.onscroll = function () {
+      if(window.pageYOffset < 1000) {
+          $('.' + id).fadeOut(300);
+      }
+      else {
+          $('.' + id).fadeIn(300);
+      }
+  }
+
+	/*
+   * Button scroll menu
+   */
+  $('.js-scrollmenu').click(function(){
+      if ( $( this ).hasClass( "active" ) ) {
+      	$('.main-nav').animate({ scrollLeft: "-=350" }, 350);
+      	$( this ).toggleClass("active");
+			} else {
+      	$('.main-nav').animate({ scrollLeft: "+=350" }, 350);
+      	$( this ).toggleClass("active");
+      }
+  });
+
+  /*
+   * Button scroll block
+   */
+
+  if ( $('.js-scrollblock').length ) {
+    var firstItem = $('a.card:first-of-type');
+    var fip = firstItem.position().left;
+    var fipi = fip; // initial offset for first card
+  }
+
+  $('.js-scrollblock').click(function(){
+    var wrapper = $('.block__scroll-wrapper');
+    var ww = wrapper[0].scrollWidth;
+    var sw = $( window ).width();
+    var $this = $(this);
+
+
+    if ( $this.hasClass( "reverse" ) ) {
+
+      wrapper.animate({ scrollLeft: "-=" + (sw - (fipi / 2) ) },
+        { complete: function(){
+            var fip = firstItem.position().left;
+            // console.log("new fip = " + ( -fip - fipi ));
+
+            if ( ( fip - fipi ) == 0 ) {
+              $this.toggleClass("reverse");
+            }
+          }
+        },
+      500);
+
+    } else {
+
+      wrapper.animate({ scrollLeft: "+=" + (sw - (fipi / 2) ) },
+        { complete: function(){
+            var fip = firstItem.position().left;
+            // console.log("new fip = " + ( -fip + fipi ));
+            // console.log("result = " + (( -fip + fipi) >= ( ww - sw )) );
+
+            if ( ( -fip + fipi) == ( ww - sw )  ) {
+              $this.toggleClass("reverse");
+            }
+          }
+        }, 
+      500);
     }
 
-		/*
-     * Button scroll menu
-     */
-    $('.js-scrollmenu').click(function(){
-        if ( $( this ).hasClass( "active" ) ) {
-        	$('.main-nav').animate({ scrollLeft: "-=350" }, 350);
-        	$( this ).toggleClass("active");
-				} else {
-        	$('.main-nav').animate({ scrollLeft: "+=350" }, 350);
-        	$( this ).toggleClass("active");
-        }
-    });
-
-    /*
-     * Button scroll block
-     */
-
-    if ( $('.js-scrollblock').length ) {
-      var firstItem = $('a.card:first-of-type');
-      var fip = firstItem.position().left;
-      var fipi = fip; // initial offset for first card
-    }
-
-    $('.js-scrollblock').click(function(){
-      var wrapper = $('.block__scroll-wrapper');
-      var ww = wrapper[0].scrollWidth;
-      var sw = $( window ).width();
-      var $this = $(this);
-
-
-        if ( $this.hasClass( "reverse" ) ) {
-
-          wrapper.animate({ scrollLeft: "-=" + (sw - (fipi / 2) ) },
-            { complete: function(){
-                var fip = firstItem.position().left;
-                // console.log("new fip = " + ( -fip - fipi ));
-
-                if ( ( fip - fipi ) == 0 ) {
-                  $this.toggleClass("reverse");
-                }
-              }
-            },
-          500);
-
-        } else {
-
-          wrapper.animate({ scrollLeft: "+=" + (sw - (fipi / 2) ) },
-            { complete: function(){
-                var fip = firstItem.position().left;
-                // console.log("new fip = " + ( -fip + fipi ));
-                // console.log("result = " + (( -fip + fipi) >= ( ww - sw )) );
-
-                if ( ( -fip + fipi) == ( ww - sw )  ) {
-                  $this.toggleClass("reverse");
-                }
-              }
-            }, 
-          500);
-        }
-
-        // console.log( "firstitem position - firstitem initial position = " + (fip - fipi) );
-        // console.log( "wrapperwidth - screenwidth = " + (ww - sw) );
-        // document.activeElement = null;
-    });
+    // console.log( "firstitem position - firstitem initial position = " + (fip - fipi) );
+    // console.log( "wrapperwidth - screenwidth = " + (ww - sw) );
+    // document.activeElement = null;
+  });
 
 });
