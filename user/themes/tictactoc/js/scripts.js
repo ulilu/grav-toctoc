4d5d079af3192389e6c9a6c6bda94f2b9ece90e5
Uli Straub <uli.straub@kontinui.de>
2016-04-10 12:18:26 +0200

    rge

--- user/themes/tictactoc/js/scripts.js
+++ user/themes/tictactoc/js/scripts.js
@@ -69,13 +69,13 @@ $(document).ready(function() {
    */
   $(function(){
     $(".js-show-info").click(function(e) {
-        $(this).parent(".text-item").next(".text-row").toggle(300);
+        $(this).closest(".text-item").nextAll(".text-row:first").toggle(300);
         $(".js-hide-info").css('display','inline-block');
         $(this).css('display','none');
         e.preventDefault();
     });
     $(".js-hide-info").click(function(e) {
-        $(this).parent(".text-item").next(".text-row").hide(300);
+        $(this).closest(".text-item").nextAll(".text-row:first").hide(300);
         $(".js-show-info").css('display','inline-block');
         $(".js-hide-info").css('display','none');
         e.preventDefault();
