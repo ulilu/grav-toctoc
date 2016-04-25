117aa3b689bf86e06493c9cc725551a2c59cdd39
Uli Straub <uli.straub@kontinui.de>
2016-04-25 23:45:56 +0200

    dfasf

--- user/themes/tictactoc/js/scripts.js
+++ user/themes/tictactoc/js/scripts.js
@@ -104,17 +104,17 @@ $(document).ready(function() {
 
 
   /*
-   * Trigger "Mehr Info / Weniger Info" in .category-content on list pages
+   * Trigger "plus de details / moins de details" in .category-content on list pages
    */
   $(function(){
     $(".js-show-info").click(function(e) {
-        $(this).closest(".text-item").nextAll(".text-row:first").toggle().addClass('is-shown');
+        $(this).closest(".js-info-toggle-btns").nextAll(".js-info-to-toggle:first").toggle().addClass('is-shown');
         $(this).next(".js-hide-info").css('display','inline-block');
         $(this).css('display','none');
         e.preventDefault();
     });
     $(".js-hide-info").click(function(e) {
-        $(this).closest(".text-item").nextAll(".text-row:first").hide().toggleClass('is-shown');
+        $(this).closest(".js-info-toggle-btns").nextAll(".js-info-to-toggle:first").hide().toggleClass('is-shown');
         $(".js-show-info").css('display','inline-block');
         $(".js-hide-info").css('display','none');
         e.preventDefault();
