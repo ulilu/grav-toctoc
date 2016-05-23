2d1c3e72286a014a335d2b3b1b2d954cce4ac520
Uli Straub <uli.straub@kontinui.de>
2016-05-23 14:18:05 +0200

    ads

--- user/themes/tictactoc/js/scripts.js
+++ user/themes/tictactoc/js/scripts.js
@@ -110,13 +110,13 @@ $(document).ready(function() {
     $(".js-show-info").click(function(e) {
         $(this).closest(".js-info-toggler").nextAll(".js-info-to-toggle:first").toggle().addClass('is-shown');
         $(this).next(".js-hide-info").css('display','inline-block');
-        $(this).css('display','none');
+        $(this).css('display','none !important');
         e.preventDefault();
     });
     $(".js-hide-info").click(function(e) {
         $(this).closest(".js-info-toggler").nextAll(".js-info-to-toggle:first").hide().toggleClass('is-shown');
         $(".js-show-info").css('display','inline-block');
-        $(".js-hide-info").css('display','none');
+        $(".js-hide-info").css('display','none !important');
         e.preventDefault();
     });
 });
