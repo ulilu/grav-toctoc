4e4cbcfcf3d346e5664a079f59f4e9b36ed78e16
Uli Straub <uli.straub@kontinui.de>
2016-05-26 16:25:30 +0200

    ads

--- user/themes/tictactoc/js/scripts.js
+++ user/themes/tictactoc/js/scripts.js
@@ -103,108 +103,33 @@ $(document).ready(function() {
   accordionSlide();
 
 
+  $(function(){
+    var $optionSets = $('.filter'),
+    $optionLinks = $optionSets.find('a');
 
+    $optionLinks.click(function(){
 
-// $(window).load(function() {  
-    // var $container = $('#container');
-    // //Run to initialise column sizes
-    // updateSize();
+      var $this = $(this);
+      // don't proceed if already selected
+      if ( $this.hasClass('selected') ) {
+        return false;
+      }
+ 
+      var $optionSet = $this.parents('.filter');
+      $optionSet.find('.selected').removeClass('selected');
+      $this.addClass('selected');
 
-    // //Load masonry when images all loaded
-    // $container.imagesLoaded( function(){
+      // make option object dynamically, i.e. { filter: '.my-filter-class' }
+      var options = {},
+      key = $optionSet.attr('data-option-key'),
+      value = $this.attr('data-option-value');
+      // parse 'false' as false boolean
+      value = value === 'false' ? false : value;
+      options[ key ] = value;
 
-    //     $container.isotope({
-    //         // options
-    //         itemSelector : '.element',  
-    //         layoutMode : 'masonry',
-    //         transformsEnabled: true,
-    //         columnWidth: function( containerWidth ) {
-    //             containerWidth = $browserWidth;
-    //             return Math.floor(containerWidth / $cols);
-    //         }
-    //     });
-    // });
-    
-    //     // update columnWidth on window resize
-    //     $(window).smartresize(function(){  
-    //         updateSize();
-    //         $container.isotope( 'reLayout' );
-    //     });
-
-    // //Set item size
-    // function updateSize() {
-    //     $browserWidth = $container.width();
-    //     $cols = 4;
-
-    //     if ($browserWidth >= 1170) {
-    //         $cols = 4;
-    //     }
-    //     else if ($browserWidth >= 800 && $browserWidth < 1170) {
-    //         $cols = 3;
-    //     }
-    //     else if ($browserWidth >= 480 && $browserWidth < 800) {
-    //         $cols = 2;
-    //     }
-    //     else if ($browserWidth >= 320 && $browserWidth < 480) {
-    //         $cols = 1;
-    //     }
-    //     else if ($browserWidth < 401) {
-    //         $cols = 1;
-    //     }
-    //     //console.log("Browser width is:" + $browserWidth);
-    //     //console.log("Cols is:" + $cols);
-
-    //     // $gutterTotal = $cols * 20;
-    //     $browserWidth = $browserWidth; // - $gutterTotal;
-    //     $itemWidth = $browserWidth / $cols;
-    //     $itemWidth = Math.floor($itemWidth);
-
-    //     $(".element").each(function(index){
-    //         $(this).css({"width":$itemWidth+"px"});             
-    //     });
-
-
-
-        var $optionSets = $('.filter'),
-        $optionLinks = $optionSets.find('a');
-
-        $optionLinks.click(function(){
-
-          var $this = $(this);
-          // don't proceed if already selected
-          if ( $this.hasClass('selected') ) {
-            return false;
-          }
-     
-          var $optionSet = $this.parents('.filter');
-          $optionSet.find('.selected').removeClass('selected');
-          $this.addClass('selected');
-
-          // make option object dynamically, i.e. { filter: '.my-filter-class' }
-          var options = {},
-          key = $optionSet.attr('data-option-key'),
-          value = $this.attr('data-option-value');
-          // parse 'false' as false boolean
-          value = value === 'false' ? false : value;
-          options[ key ] = value;
-      //   if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
-      //     // changes in layout modes need extra logic
-      //     changeLayoutMode( $this, options )
-      // } else {
-      //     // otherwise, apply new options
-      //     $container.isotope( options );
-      // }
-
-          return false;
-      });       
-
-    // };
-
-    //     // Initialize the gallery
-    //     $('.thumb').touchTouch();
-
-    // });
-
+      return false;
+    });       
+  });
 
 
 
@@ -224,7 +149,7 @@ $(document).ready(function() {
         $(this).css('display','none');
         e.preventDefault();
     });
-});
+  });
 
 
   jQuery(".row-with-title figcaption").fitText(0.4, { minFontSize: '24px', maxFontSize: '42px' });
