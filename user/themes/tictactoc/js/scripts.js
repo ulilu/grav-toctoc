1b7535e4feedd0739fa5ee139d271e7129499dad
Uli Straub <uli.straub@kontinui.de>
2016-06-08 01:28:56 +0200

    asd

--- user/themes/tictactoc/js/scripts.js
+++ user/themes/tictactoc/js/scripts.js
@@ -1,23 +1,5 @@
 
 /*
-var  mn = $("navigation");
-    mns = "navigation--scrolled";
-    hdr = $('.header').height();
-
-$(function(){
-    $(window).scroll(function() {
-        if ($(this).scrollTop() >= 101) {
-            $('.nav-bar').addClass('is-sticky');
-        }
-        else {
-            $('.nav-bar').removeClass('is-sticky');
-        }
-    });
-});
-*/
-
-
-/*
  * Document ready start
  */
 
