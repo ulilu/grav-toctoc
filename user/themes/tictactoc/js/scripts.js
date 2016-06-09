72b1421c2746cc8160dcaddb598bfa3d1d3ee50f
Uli Straub <uli.straub@kontinui.de>
2016-06-09 04:50:37 +0200

    asdf

--- user/themes/tictactoc/js/scripts.js
+++ user/themes/tictactoc/js/scripts.js
@@ -287,14 +287,19 @@ $("nav").headroom({
 
   var scrollContents = function() {
 
+    if ( $('.js-scrollblock').length ) {
       var firstItem = $('a.card:first-of-type');
       var fip = firstItem.position().left;
       var fipi = fip; // initial offset for first card
+      console.log("first ever fipi = " + fipi);
+    }
 
     $('.js-scrollblock').click(function(){
       var wrapper = $('.block__scroll-wrapper');
       var ww = wrapper[0].scrollWidth;
+      console.log("wrapper scrollWidth = " + ww);
       var sw = $( window ).width();
+      console.log("window width = " + sw);
       var $this = $(this);
 
 
@@ -303,7 +308,7 @@ $("nav").headroom({
         wrapper.animate({ scrollLeft: "-=" + (sw - (fipi / 2) ) },
           { complete: function(){
               var fip = firstItem.position().left;
-              // console.log("new fip = " + ( -fip - fipi ));
+              console.log("new fip after scroll left = " + ( -fip - fipi ));
 
               if ( ( fip - fipi ) == 0 ) {
                 $this.toggleClass("reverse");
@@ -317,8 +322,8 @@ $("nav").headroom({
         wrapper.animate({ scrollLeft: "+=" + (sw - (fipi / 2) ) },
           { complete: function(){
               var fip = firstItem.position().left;
-              // console.log("new fip = " + ( -fip + fipi ));
-              // console.log("result = " + (( -fip + fipi) >= ( ww - sw )) );
+              console.log("new fip after scroll right = " + ( -fip + fipi ));
+              console.log("result = " + (( -fip + fipi) >= ( ww - sw )) );
 
               if ( ( -fip + fipi) == ( ww - sw )  ) {
                 $this.toggleClass("reverse");
