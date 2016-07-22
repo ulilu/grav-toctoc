33727cfb0e66e5a0df7dd6c5fcad8ac7aa95fc07
Uli Straub <uli.straub@kontinui.de>
2016-07-22 18:59:44 +0200

    sdcs

--- user/themes/tictactoc/js/msie.js
+++ user/themes/tictactoc/js/msie.js
@@ -2,12 +2,14 @@
 
 /*
  *  HOT DANG! Apparently, IE9 doesn't understand console.log per se.
- *  Unless you open the dev tools - after that all is good. Found here:
+ *  Unless you open the dev tools - after that all is good. 
+ *  Found the following articles and fixes:
  *  http://thisbythem.com/blog/whats-up-with-the-js-console-in-ie9/
  *  https://stackoverflow.com/questions/5472938/does-ie9-support-console-log-and-is-it-a-real-function
  */
 if (!window.console) window.console = {};
 if (!window.console.log) window.console.log = function () { };
+
 
 /*
  *  Polyfill to add leverage background-position to imitate object-fit
