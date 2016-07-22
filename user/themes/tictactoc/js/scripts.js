f24b0d396e52cec2d8386fc495b6bae698f68986
Uli Straub <uli.straub@kontinui.de>
2016-07-22 04:05:25 +0200

    asdcd

--- user/themes/tictactoc/js/scripts.js
+++ user/themes/tictactoc/js/scripts.js
@@ -48,12 +48,13 @@ $(document).ready(function() {
       html.className += " fonts-loaded";
       createCookie('fonts-loaded', '1', 1); 
       /* sessionStorage.fontsLoaded = true; */
-    }).catch(function () {
-      console.log('Fontawesome loading failed, using fallback font');
-      html.className -= " fonts-loading";
-      html.className += " fonts-failed";
-      /* sessionStorage.fontsLoaded = false; */
     });
+    // .catch(function () {
+    //   console.log('Fontawesome loading failed, using fallback font');
+    //   html.className -= " fonts-loading";
+    //   html.className += " fonts-failed";
+    //   /* sessionStorage.fontsLoaded = false; */
+    // });
   });
 
 
