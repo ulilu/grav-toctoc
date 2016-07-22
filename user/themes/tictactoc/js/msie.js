b8675a5c396f9d7ea0ded80f01e28e2ebb887135
Uli Straub <uli.straub@kontinui.de>
2016-07-22 13:23:28 +0200

    adc

--- user/themes/tictactoc/js/msie.js
+++ user/themes/tictactoc/js/msie.js
@@ -117,6 +117,29 @@ document.addEventListener("DOMContentLoaded", function(event) {
         // Add background-position: center center
         container[i].style.backgroundPosition = 'center center';
       }
+
+    } else if ( document.querySelector('figure a') ) {
+
+      var container = document.querySelectorAll('figure a');
+      
+      // Loop through HTMLCollection
+      for(var i = 0; i < container.length; i++) {
+        
+        // Asign image source to variable
+        var imageSource = container[i].querySelector('img').src;
+        
+        // Hide image
+        container[i].querySelector('img').style.opacity = '0';
+        
+        // Add background-size: cover
+        container[i].style.backgroundSize = 'cover';
+        
+        // Add background-image: and put image source here
+        container[i].style.backgroundImage = 'url(' + imageSource + ')';
+        
+        // Add background-position: center center
+        container[i].style.backgroundPosition = 'center center';
+      }
     }
 
   } else {
