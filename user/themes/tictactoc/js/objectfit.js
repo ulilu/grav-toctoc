0a890da9f410f7c8e36fc7f73faeaa46e3ef8558
Uli Straub <uli.straub@kontinui.de>
2016-07-31 02:31:22 +0200

    asc

--- user/themes/tictactoc/js/objectfit.js
+++ user/themes/tictactoc/js/objectfit.js
@@ -152,7 +152,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var imageSource = container[i].querySelector('img').getAttribute("data-src");
         
         // Hide image
-        container[i].querySelector('img').attribute.src = imageSource;
+        container[i].querySelector('img').style.content = imageSource;
         
         container[i].querySelector('img').style.opacity = '0';
 
